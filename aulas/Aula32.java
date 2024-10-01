8d5d584f980f1373e96b5a8cdb62e68702d49bc4
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2024-10-01 19:55:09 -0300

    Update Aula32.java

--- aulas/Aula32.java
+++ aulas/Aula32.java
@@ -8,11 +8,11 @@ public class Aula32 {
         System.out.println("== Arquivos ==");
 
         //ex01(); // criação e escrita
-        ex02();
+        calculaMediaNnumeros();
     }
 
     public static void ex01(){
-        String nome_arq = "numeros.txt";
+        String nome_arq = "/numeros.txt";
         try{
             FileWriter arquivo = new FileWriter(nome_arq);
 
@@ -28,7 +28,7 @@ public class Aula32 {
     }
 
     public static void ex02(){
-        String nome_arq = "numeros.txt";
+        String nome_arq = "arquivos/valores.txt";
         try{
             File arquivo = new File(nome_arq);
 
@@ -36,6 +36,49 @@ public class Aula32 {
 
             int val = entrada.nextInt();
             System.out.println(val);
+
+            entrada.close();
+        } catch (Exception e){
+            System.err.println("Erro");
+            System.err.println(e);
+        }
+    }
+
+    public static void calculaMedia(){
+        String nome_arq = "arquivos/valores.txt";
+        try{
+            int soma = 0;
+            File arquivo = new File(nome_arq);
+
+            Scanner entrada = new Scanner(arquivo);
+
+            for (int i = 0; i < 10; i++){
+                soma += (int) entrada.nextInt();
+            }
+            
+            System.out.println(soma/10);
+
+            entrada.close();
+        } catch (Exception e){
+            System.err.println("Erro");
+            System.err.println(e);
+        }
+    }
+
+    public static void calculaMediaNnumeros(){
+        String nome_arq = "arquivos/valores_n.txt";
+        try{
+            int soma = 0;
+            File arquivo = new File(nome_arq);
+
+            Scanner entrada = new Scanner(arquivo);
+
+            int n = entrada.nextInt();
+            for (int i = 0; i < n; i++){
+                soma += (int) entrada.nextInt();
+            }
+            
+            System.out.println(soma/n);
 
             entrada.close();
         } catch (Exception e){
