e19135ab653d99f3116b112072c9d7b10e1a0a0e
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2024-10-04 22:36:39 -0300

    Update AED2024_3B_T01.java

--- Trabalho/AED2024_3B_T01.java
+++ Trabalho/AED2024_3B_T01.java
@@ -1,6 +1,23 @@
 package Trabalho;
 
 public class AED2024_3B_T01 {
+    public static void mostrarMatriz(int[][] arr){
+        //int[][] arr = new int[5][5];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("[");
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j]);
+                if (j < arr[i].length - 1) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.print("]\n");
+            // if (i < arr.length -1){
+            //     System.out.print(",\n");
+            // }
+        } 
+    }
+    
     public static void mostrarVetLogico(boolean vet[]){
         System.out.print("[");
         for (int i = 0; i < vet.length; i++) {
@@ -183,4 +200,12 @@ public class AED2024_3B_T01 {
         }
         return result;
     }
+
+    public static int [][] matrizTranspor(int[][] matrizA){
+        int[][] transposta = new int[matrizA[0].length][matrizA.length];
+
+
+        return transposta;
+    }
+
 }
