cc6eb0698c424b54fc820cc4e9617730b466ceca
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2024-10-04 21:37:41 -0300

    Create Aula33.java

---
+++ aulas/Aula33.java
@@ -0,0 +1,12 @@
+package aulas;
+
+public class Aula33 {
+    public static void aula() {
+        System.out.println("== Aula 32 - Trabalho ==");
+
+        int[] vet = {1, 2, 3, 4, 5, 6, 7, 3};
+
+        int[] saida= Trabalho.AED2024_3B_T01.filtrarMaiores(vet, 9);
+        Trabalho.AED2024_3B_T01.mostrarVetInt(saida);
+    }
+}
