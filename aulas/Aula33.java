6fe4cb607d55555a79a54d6d008a558d17c792c7
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2024-10-04 22:27:52 -0300

    Update Aula33.java

--- aulas/Aula33.java
+++ aulas/Aula33.java
@@ -2,11 +2,13 @@ package aulas;
 
 public class Aula33 {
     public static void aula() {
-        System.out.println("== Aula 32 - Trabalho ==");
+        System.out.println("== Aula 33 - Trabalho ==");
 
-        int[] vet = {1, 2, 3, 4, 5, 6, 7, 3};
+        int[] vet = {1,1, 4};
+        int[] vet2 = {2, 1, 2, 3};
 
-        int[] saida= Trabalho.AED2024_3B_T01.filtrarMaiores(vet, 9);
+        int[] saida= Trabalho.AED2024_3B_T01.diferencaVetA(vet, vet2);
         Trabalho.AED2024_3B_T01.mostrarVetInt(saida);
+        System.out.println();
     }
 }
